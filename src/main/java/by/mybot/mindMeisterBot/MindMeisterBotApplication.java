ff3d61faea29318301ee865ec537e8db20aeefa5
olgaz <olgaz.sweetheart@gmail.com>
2021-07-27 12:48:39 +0300

    Initial commit

---
+++ src/main/java/by/mybot/mindMeisterBot/MindMeisterBotApplication.java
@@ -0,0 +1,13 @@
+package by.mybot.mindMeisterBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindMeisterBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindMeisterBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/mybot/mindMeisterBot/MindMeisterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mybot.mindMeisterBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindMeisterBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
